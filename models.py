1532c3015fa4494285ffcb86bc5b46928a5da54b
Matthew Moyer <matthewrmoyer@gmail.com>
2017-12-13 16:34:36 -0700

    copy enviropi

--- models.py
+++ models.py
@@ -1,47 +1,19 @@
 from app import db
 
 
-class Risk(db.Model):
+class Property(db.Model):
     """docstring for Property"""
 
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String)
-    user = db.Column(db.String)
+    location = db.Column(db.String)
+    temperature = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, server_default=db.func.now())
+    image = db.Column(db.String)
+    # imageb = db.Column(db.LargeBinary)
 
-
-    def __init__(self, type, user):
-        self.type = type
-        self.user = user
-
-
-import sqlite3
-
-def drop_table():
-    with sqlite3.connect('risks.db') as connection:
-        c = connection.cursor()
-        c.execute("""DROP TABLE IF EXISTS risk_types;""")
-        c.execute("""DROP TABLE IF EXISTS risk_fields;""")
-    return True
-
-
-def create_db():
-    with sqlite3.connect('risks.db') as connection:
-        c = connection.cursor()
-        types = """CREATE TABLE risk_types(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            type TEXT NOT NULL,
-            user TEXT DEFAULT 'anonymous'
-        );"""
-        fields = """CREATE TABLE risk_fields(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            risk_type TEXT NOT NULL,
-            data_type TEXT NOT NULL
-        );"""
-        c.execute(types)
-        c.execute(fields)
-    return True
-
-if __name__ == '__main__':
-    drop_table()
-    create_db()
+    def __init__(self, location, temperature, image):
+        self.location = location
+        self.temperature = temperature
+        self.image = image
+        # self.imageb = imageb
+        # will have to fix this
