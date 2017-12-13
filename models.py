6cbb70d7e29103ad301372c37b8725de31140a8e
Matthew Moyer <matthewrmoyer@gmail.com>
2017-12-13 16:43:39 -0700

    copy enviropi

--- models.py
+++ models.py
@@ -1,19 +1,11 @@
 from app import db
 
 
-class Property(db.Model):
+class Risk(db.Model):
     """docstring for Property"""
 
     id = db.Column(db.Integer, primary_key=True)
-    location = db.Column(db.String)
-    temperature = db.Column(db.Integer)
-    timestamp = db.Column(db.DateTime, server_default=db.func.now())
-    image = db.Column(db.String)
-    # imageb = db.Column(db.LargeBinary)
+    type = db.Column(db.String)
 
-    def __init__(self, location, temperature, image):
-        self.location = location
-        self.temperature = temperature
-        self.image = image
-        # self.imageb = imageb
-        # will have to fix this
+    def __init__(self, type):
+        self.type = type
