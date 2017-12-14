1cd2f861a7241c9178ffc0ad6c26b86d8dea3bc0
Matthew Moyer <matthewrmoyer@gmail.com>
2017-12-14 10:48:00 -0700

    refactor models

--- models.py
+++ models.py
@@ -6,8 +6,8 @@ class Risk(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String)
 
-    def __init__(self, type):
-        self.type = type
+    # def __init__(self, type):
+    #     self.type = type
 
 
 class Field(db.Model):
@@ -18,7 +18,7 @@ class Field(db.Model):
     risk_type = db.Column(db.String)
     data_type = db.Column(db.String)
     
-    def __init__(self, name, risk_type, data_type):
-        self.name = name
-        self.risk_type = risk_type
-        self.data_type = data_type
+    # def __init__(self, name, risk_type, data_type):
+    #     self.name = name
+    #     self.risk_type = risk_type
+    #     self.data_type = data_type
