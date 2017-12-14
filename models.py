cc8443043ac467eb091836ea43d86a395c7c5a6e
Matthew Moyer <matthewrmoyer@gmail.com>
2017-12-14 10:51:58 -0700

    field table

--- models.py
+++ models.py
@@ -10,15 +10,15 @@ class Risk(db.Model):
         self.type = type
 
 
-# class Field(db.Model):
-#     """docstring for Property"""
+class Field(db.Model):
+    """docstring for Property"""
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String)    
-#     risk_type = db.Column(db.String)
-#     data_type = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)    
+    risk_type = db.Column(db.String)
+    data_type = db.Column(db.String)
     
-#     def __init__(self, name, risk_type, data_type):
-#         self.name = name
-#         self.risk_type = risk_type
-#         self.data_type = data_type
+    def __init__(self, name, risk_type, data_type):
+        self.name = name
+        self.risk_type = risk_type
+        self.data_type = data_type
