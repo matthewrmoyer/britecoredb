79731b334ca7c37b1fa4f366867bf9a127fc096d
Matthew Moyer <matthewrmoyer@gmail.com>
2017-12-14 10:28:26 -0700

    add field model and seed

--- models.py
+++ models.py
@@ -8,3 +8,17 @@ class Risk(db.Model):
 
     def __init__(self, type):
         self.type = type
+
+
+class Field(db.Model):
+    """docstring for Property"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)    
+    risk_type = db.Column(db.String)
+    data_type = db.Column(db.String)
+    
+    def __init__(self, type):
+        self.name = name
+        self.risk_type = risk_type
+        self.data_type = data_type
