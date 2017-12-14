f961a15f5e2d615d3874edc9e327204807476057
Matthew Moyer <matthewrmoyer@gmail.com>
2017-12-14 16:01:55 -0700

    setup single risk endpoint

--- app.py
+++ app.py
@@ -88,15 +88,17 @@ def fields_collection():
 @app.route('/risks/<risk_id>', methods=['GET'])
 def risk_resource(risk_id):
     if request.method == 'GET':
-        all_risks = []
+        data = []
         risk = models.Risk.query.filter_by(id=risk_id).first()
-        all_risks.append({
+        fields = models.Field.query.filter_by(risk_type=risk.type).all()
+        data.append({
             'risk': {
             'id': risk.id,
             'type': risk.type,
-            }
+            },
+            'fields': fields
         })
-        return jsonify(all_risks)
+        return jsonify(data)
     # if request.method == 'GET':
     #     data = []
     #     risk = models.Risk.query.filter_by(id=risk_id).first()
