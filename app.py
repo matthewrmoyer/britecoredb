51923ac045b376bd29d9d256632e0e0a341c0cbf
Matthew Moyer <matthewrmoyer@gmail.com>
2017-12-14 14:26:18 -0700

    setup cors for posts

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ from flask_cors import CORS, cross_origin
 
 print('app.py working')
 app = Flask(__name__)
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app, resources=r'/api/*', headers='Content-Type')
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost:5432/britecore_db'
 # UPLOAD_FOLDER = './fileuploadfolder'
